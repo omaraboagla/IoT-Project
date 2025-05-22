b62d287bed01a87687d12294ad8152b8c693d269
ahmedyasser002 <ay012941@gmail.com>
2025-05-23 00:47:39 +0300

    adding alerts

--- backend/src/main/java/com/iotproject/iotproject/Repo/TrafficSensorRepository.java
+++ backend/src/main/java/com/iotproject/iotproject/Repo/TrafficSensorRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TrafficSensorRepository extends JpaRepository<TrafficSensor, UUID>, JpaSpecificationExecutor<TrafficSensor> {
+    Optional<TrafficSensor> findTopByOrderByTimestampDesc();
+
 }
 
